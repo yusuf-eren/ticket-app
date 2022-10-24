3eb827a2116169f97c8dcf635b654e92d7a7ca4d
yusuf-eren <erenyusuf170@gmail.com>
2022-10-24 04:34:39 +0300

    error handling and some more stuff

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,14 +1,17 @@
-import { NextFunction, Request, Response } from 'express';
+import { Request, Response, NextFunction } from "express";
+import { CustomError } from "../errors/custom-error";
 
 export const errorHandler = (
-    err: Error,
-    req: Request,
-    res: Response,
-    next: NextFunction
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
 ) => {
-    console.log('Something Went wrong', err);
+  if (err instanceof CustomError) {
+    return res.status(err.statusCode).send({ errors: err.serializeErrors() });
+  }
 
-    res.status(400).send({
-        message: 'Something went wrong',
-    });
+  res.status(400).send({
+    errors: [{ message: "Something went wrong" }],
+  });
 };
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,30 +1,31 @@
-import express, { Request, Response } from 'express';
-import { body, validationResult } from 'express-validator';
+import express, { Request, Response } from "express";
+import { body, validationResult } from "express-validator";
+import { RequestValidationError } from "../errors/request-validation-error";
+import { DatabaseConnectionError } from "../errors/database-connection-error";
 
 const router = express.Router();
 
 router.post(
-    '/api/users/signup',
-    [
-        body('email').isEmail().withMessage('Email must be valid'),
-        body('password')
-            .trim()
-            .isLength({ min: 4, max: 20 })
-            .withMessage('Password must be between 4 and 20 characterss'),
-    ],
-    (req: Request, res: Response) => {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) return res.status(400).send(errors.array());
-        const { email, password } = req.body;
+  "/api/users/signup",
+  [
+    body("email").isEmail().withMessage("Email must be valid"),
+    body("password")
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .withMessage("Password must be between 4 and 20 characters"),
+  ],
+  async (req: Request, res: Response) => {
+    const errors = validationResult(req);
 
-        if (!email || typeof email !== 'string') {
-            res.status(400).send('Provide a valid email');
-        }
-        console.log('Creating a user...');
-
-        res.send({})
-        // new User({ email, password })
+    if (!errors.isEmpty()) {
+      throw new RequestValidationError(errors.array());
     }
+
+    console.log("Creating a user...");
+    throw new DatabaseConnectionError();
+
+    res.send({});
+  }
 );
 
 export { router as signupRouter };
