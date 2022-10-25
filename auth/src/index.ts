c353e804b0b45b7d3dd3c64b34ad4f2b788cfb36
yusuf-eren <erenyusuf170@gmail.com>
2022-10-25 03:05:18 +0300

    connection string

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
+import mongoose from 'mongoose';
 
 import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
@@ -18,11 +19,22 @@ app.use(signoutRouter);
 app.use(signupRouter);
 
 app.all('*', async (req, res) => {
-  throw new NotFoundError();
+    throw new NotFoundError();
 });
 
 app.use(errorHandler);
 
+const start = async () => {
+    try {
+        await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
+        console.log("Successfully connected to db")
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+start();
+
 app.listen(3000, () => {
-  console.log('Listening on port 3000!!!!!!!!');
+    console.log('Listening on port 3000!!!!!!!!');
 });
