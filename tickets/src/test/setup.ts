9f0e61ef437147377932ea0d6b6ff49c5e8b2902
yusuf-eren <erenyusuf170@gmail.com>
2022-11-01 08:44:16 +0300

    settiing up microservice testing

--- tickets/src/test/setup.ts
+++ tickets/src/test/setup.ts
@@ -2,9 +2,10 @@ import request from 'supertest';
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
 import { app } from '../app';
+import jwt from 'jsonwebtoken';
 
 declare global {
-    var signin: () => Promise<string[]>;
+    var signin: () => string[];
 }
 
 let mongo: any;
@@ -32,15 +33,20 @@ afterAll(async () => {
     await mongoose.connection.close();
 });
 
-global.signin = async () => {
-    const email = 'test@test.com';
-    const password = 'password';
-
-    const response = await request(app)
-        .post('/api/users/signup')
-        .send({ email, password })
-        .expect(201);
-    const cookie = response.get('Set-Cookie');
-
-    return cookie;
+global.signin = () => {
+    // Build a JWT payload. { id, email }
+    const payload = {
+        id: '123',
+        email: 'test@test.com',
+    };
+    // Create the JWT
+    const token = jwt.sign(payload, process.env.JWT_KEY!);
+    // Build session Object. { jwt: MY_JWT }
+    const session = { jwt: token };
+    // Turn that session into JSON
+    const sessionJSON = JSON.stringify(session);
+    // Take JSON and encode it as base64
+    const base64 = Buffer.from(sessionJSON).toString('base64');
+    // return a string thats the cookie with the encoded data
+    return [`session=${base64}`];
 };
