588bc60576b1f55ab0bf4fc0b319e4e12de2b8f7
yusuf-eren <erenyusuf170@gmail.com>
2022-11-01 09:37:28 +0300

    ticket crud operations

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -1,10 +1,32 @@
 import express, { Request, Response } from 'express';
-import { requireAuth } from '@yusuferen/common';
+import { requireAuth, validateRequest } from '@yusuferen/common';
+import { body } from 'express-validator';
+import { Ticket } from '../models/ticket';
 
 const router = express.Router();
 
-router.post('/api/tickets', requireAuth, (req: Request, res: Response) => {
-    res.status(200).send('hi');
-});
+router.post(
+    '/api/tickets',
+    requireAuth,
+    [
+        // Title empty check
+        body('title').not().isEmpty().withMessage('Title is required'),
+        // Price greater than 0 check
+        body('price')
+            .isFloat({ gt: 0 })
+            .withMessage('Price must be greater  than 0'),
+    ],
+    validateRequest,
+    async (req: Request, res: Response) => {
+        const { title, price } = req.body;
+        const ticket = Ticket.build({
+            title,
+            price,
+            userId: req.currentUser!.id,
+        });
+        await ticket.save();
+        res.status(201).send(ticket);
+    }
+);
 
 export { router as createTicketRouter };
