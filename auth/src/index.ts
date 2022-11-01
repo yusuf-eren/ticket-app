f4faa96b78071abd82b2a9b3310bafa1e5b34cad
yusuf-eren <erenyusuf170@gmail.com>
2022-11-01 08:21:48 +0300

    env variable check

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,8 +2,9 @@ import mongoose from 'mongoose';
 import { app } from './app';
 
 const start = async () => {
+    if (!process.env.MONGO_URI) throw new Error('MONGO_URI must be defined');
     try {
-        await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
+        await mongoose.connect(process.env.MONGO_URI);
         console.log('Successfully connected to db');
     } catch (error) {
         console.log(error);
