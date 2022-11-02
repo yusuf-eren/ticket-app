c60d9a57f126e142944ceaeaeee611e33e1fa974
yusuf-eren <erenyusuf170@gmail.com>
2022-11-02 12:00:32 +0300

    update route

--- tickets/src/routes/update.ts
+++ tickets/src/routes/update.ts
@@ -10,6 +10,15 @@ import { Ticket } from '../models/ticket';
 
 const router = express.Router();
 
-router.put('/api/tickets/:id', async (req: Request, res: Response) => {});
+router.put(
+    '/api/tickets/:id',
+    requireAuth,
+    async (req: Request, res: Response) => {
+        const ticket = await Ticket.findById(req.params.id);
+        if (!ticket) throw new NotFoundError();
+
+        res.send(ticket);
+    }
+);
 
 export { router as updateTicketRouter };
