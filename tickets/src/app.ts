18513e7fd576a244e1c4f55d3aef556d7ea2560e
yusuf-eren <erenyusuf170@gmail.com>
2022-11-02 11:21:47 +0300

    routes added

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -5,6 +5,9 @@ import cookieSession from 'cookie-session';
 
 import { currentUser, errorHandler, NotFoundError } from '@yusuferen/common';
 import { createTicketRouter } from './routes/new';
+import { showTicketRouter } from './routes/show';
+import { indexTicketRouter } from './routes';
+import { updateTicketRouter } from './routes/update';
 
 const app = express();
 app.use(json());
@@ -16,6 +19,9 @@ app.use(
 
 app.use(currentUser);
 app.use(createTicketRouter);
+app.use(showTicketRouter);
+app.use(indexTicketRouter);
+app.use(updateTicketRouter);
 
 app.all('*', async (req, res) => {
     throw new NotFoundError();
