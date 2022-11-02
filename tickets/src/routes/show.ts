bdb78299f6c641582900c093f0b2d05b00e0174d
yusuf-eren <erenyusuf170@gmail.com>
2022-11-02 11:22:06 +0300

    shows route

---
+++ tickets/src/routes/show.ts
@@ -0,0 +1,13 @@
+import { NotFoundError } from '@yusuferen/common';
+import express, { Request, Response } from 'express';
+import { Ticket } from '../models/ticket';
+
+const router = express.Router();
+
+router.get('/api/tickets/:id', async (req: Request, res: Response) => {
+    const ticket = await Ticket.findById(req.params.id);
+    if (!ticket) throw new NotFoundError();
+    res.status(200).send(ticket);
+});
+
+export { router as showTicketRouter };
